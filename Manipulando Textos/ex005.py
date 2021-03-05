eaea6c76a82efc1d43222b37e16403ec589871ed
Camila Fontes <camiladejesusfontes@gmail.com>
2021-03-05 17:23:59 -0300

    Usando Lower

---
+++ Manipulando Textos/ex005.py
@@ -0,0 +1,4 @@
+print(' Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.')
+nome = str(input('Digite a sua cidade de nascimento: '))
+sobrenome = 'Silva'
+print('O nome digitado foi {} e a informação é {}.'.format(nome,'Silva' in nome.lower()))
