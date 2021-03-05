4c95996743bd2e35a28b62e1434bc1f6ff8b8cce
Camila Fontes <camiladejesusfontes@gmail.com>
2021-03-05 20:29:08 -0300

    Exercício Par ou Ímpar

---
+++ Condições/ex004.py
@@ -0,0 +1,8 @@
+print('-=-' * 28)
+print("Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.")
+print('-=-'* 28)
+num = int(input('Digite o número de preferência: '))
+if num % 2:
+    print('Este número é ÍMPAR!')
+else:
+    print('O número é PAR!')
