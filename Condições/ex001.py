ef017ddb69ecd5ae7bd780d83c93f7e513f00fce
Camila Fontes <camiladejesusfontes@gmail.com>
2021-03-05 19:41:34 -0300

    Primeiro exercício com IF e ELSE

---
+++ Condições/ex001.py
@@ -0,0 +1,5 @@
+nome = str(input('Qual seu nome? '))
+if nome == 'Camila':
+    print('Olá {}, é um prazer ver você evoluindo em Python'.format(nome))
+else:
+    print('Ok, seu nome é {}, continue aprendendo e evoluindo!'.format(nome))
