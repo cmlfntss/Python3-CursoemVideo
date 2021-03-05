e56a4e644137c930f7cc48a862f6ae5dd1145b99
Camila Fontes <camiladejesusfontes@gmail.com>
2021-03-05 16:54:54 -0300

    True or false

---
+++ Manipulando Textos/ex004.py
@@ -0,0 +1,4 @@
+print(""""Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.""")
+nasc = str(input('Digite a sua cidade de nascimento: '))
+print('A informação é {}.'.format('Santo' in nasc))
+
