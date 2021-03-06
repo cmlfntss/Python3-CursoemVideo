a223aab009bc5ae2b73d00d781f77bcaa0e1d12e
Camila Fontes <camiladejesusfontes@gmail.com>
2021-03-06 11:23:06 -0300

    Maior e Menor

---
+++ Python3-Mundo02CursoemVídeo/ex004.py
@@ -0,0 +1,16 @@
+print("""Escreva um programa que leia dois números inteiros e compare-os. 
+mostrando na tela uma mensagem:
+– O primeiro valor é maior.
+– O segundo valor é maior.
+– Não existe valor maior, os dois são iguais.
+""")
+
+num1 = int(input('Qual é o primeiro número?'))
+num2 = int(input('Qual é o segundo número?'))
+
+if num1 > num2:
+    print('O PRIMEIRO número é MAIOR.')
+elif num2 > num1:
+    print('O SEGUNDO número é MAIOR.')
+else:
+    print('Os números são IGUAIS!!!')
