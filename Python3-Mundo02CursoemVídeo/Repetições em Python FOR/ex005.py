803a29c99bb4aea12dbec25a9c43832baa036642
Camila Fontes <camiladejesusfontes@gmail.com>
2021-03-07 12:12:30 -0300

    CONTADOR EM FOR

---
+++ Python3-Mundo02CursoemVídeo/Repetições em Python FOR/ex005.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+print('-=-' * 10)
+print('INICIANDO CONTAGEM REGRESSIVA: \n')
+for cont in range(10, -1, -1):
+    print(cont)
+    sleep(1)
+print('POW!', 'BUM', 'THIS IS FIREWORKS!!!')
+print('-=-' * 10)
